7cb47cb3c54221e8a73ebf2cdddb217160d6f331
diwadiwakar <diwa221998@gmail.com>
2022-05-03 20:40:18 +0530

    pushing code to branch

---
+++ src/test/java/org/get/message.java
@@ -0,0 +1,18 @@
+package org.get;
+
+public class message {
+	private void method1() {
+		System.out.println("1");
+
+	}
+
+	private void method2() {
+		System.out.println("2");
+
+	}
+
+	private void method3() {
+		System.out.println("3");
+	}
+
+}
